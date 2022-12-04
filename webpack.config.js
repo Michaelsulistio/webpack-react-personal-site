bfeb96b3ace72cf6b98edc9639280eddb3cc6d9d
Michael Sulistio <michaelsulistio@gmail.com>
2022-12-04 13:13:18 -0800

    Fixed pnp webpack plugin issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
 
 const port = process.env.PORT || 3000;
 
