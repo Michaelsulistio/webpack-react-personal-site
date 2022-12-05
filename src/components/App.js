71922ad83152f90c483c93b2224e6ced96ebc104
mike wsl <michaelsulistio@gmail.com>
2022-12-04 23:02:32 -0800

    added MUI button test component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 
 const App = () => {
   return (
       <div>
-      Hello
+      <Button variant="contained">Hello World</Button>
       </div>
   );
 };
