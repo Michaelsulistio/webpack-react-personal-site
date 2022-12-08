28d1c314e1ad8d5940e569e62cdbcf265ef7c2f1
mike wsl <michaelsulistio@gmail.com>
2022-12-07 21:19:11 -0800

    added profile picture ai art

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import CardMedia from '@mui/material/CardMedia';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Card from '@mui/material/Card';
+
+// import Item from '@mui/material/Item';
 
 
 const App = () => {
   return (
-      <div>
-      <Button variant="contained">Hello World</Button>
-      </div>
+      <Grid container spacing={4}>
+        <Grid container spacing={2}>
+          <Grid item xs={2}>
+            <Card sx={{ maxWidth: 400 }}>
+              <CardMedia
+                component="img"
+                height="400"
+                image="/static/images/neckbeard_dev.png"
+                alt="Prompt: Futuristic Full-Stack Developer"
+              />
+            </Card>
+          </Grid>
+          <Grid item xs={2}>
+            <Card sx={{ maxWidth: 400 }}>
+              <CardMedia
+                component="img"
+                height="400"
+                image="/static/images/normal_dev.jpg"
+                alt="Prompt: Futuristic Full-Stack Developer"
+              />
+            </Card>
+          </Grid>
+          <Grid item xs={2}>
+            <Card sx={{ maxWidth: 400 }}>
+              <CardMedia
+                component="img"
+                height="400"
+                image="/static/images/futuristic_dev.png"
+                alt="Prompt: Futuristic Full-Stack Developer"
+              />
+            </Card>
+          </Grid>
+        </Grid>
+      </Grid>
   );
 };
 
