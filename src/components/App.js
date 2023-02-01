05afd15774913d3fa33302b9be4a349b9e925d0e
mike wsl <michaelsulistio@gmail.com>
2023-02-01 00:47:31 -0800

    Add flex boxes and copy text

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,74 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { makeStyles } from '@mui/styles';
 import PhotoSlideshow from './PhotoSlideshow';
 
 const useStyles = makeStyles((theme) => ({
-  appContainer: {
-    backgroundColor: 'grey',
-    minHeight: '100vh',
-    minWidth: '100vh'
+  headerContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   logoHeader: {
-    textAlign: 'center'
+    color: 'black',
+    textAlign: 'center',
+    fontSize: '72px',
+    fontWeight: 'bold',
+    fontFamily: ['Inter', 'sans-serif'],
   },
+  introText: {
+    color: 'black',
+    textAlign: 'left',
+    fontSize: '18px',
+    fontFamily: ['Inter', 'sans-serif'],
+    inlineSize: '500px',
+    overflowWrap: 'break-word'
+  },
+  slideshowContainer: {
+    width: '400px',
+    height: '400px'
+  },
+  footer: {
+    fontSize: '18px',
+    textAlign: 'center'
+  }
 }));
 
 
 const App = () => {
   const classes = useStyles();
   return (
-    <Container fixed className={classes.appContainer}>
-      <div className={classes.logoHeader}>
-        michael sulist.io
-      </div>
+    <Box className={classes.headerContainer} column={1}>
+        <Box item xs={4}>
+          <Box>
+            <p className={classes.logoHeader}>Michael Sulist.io </p>
+            <p className={classes.introText}>
+              Hi! I'm a fullstack Software Engineer based in San Francisco.
+              I attended the Unviersity of Washington for Computer Science and graduated in 2020. Since then, 
+              I've worked at Meta as a fullstack Mobile developer, until I was laid off. 
+              <br /> <br />
+              I worked on the Messenger App and Facebook App with extensive experience in
+              <span style={{color: '#007AFF'}}> iOS (Objective-C/Swift)</span>,   
+              <span style={{color: '#a4c639'}}> Android(Java/Kotlin)</span>, and   
+              <span style={{color: '#61dafb'}}> React Native</span> development.
+            </p>
+          </Box>
+        </Box>
+        <Box className={classes.slideshowContainer} item xs={4}>
+          <PhotoSlideshow />
+        </Box>
+      {/* <Box className={classes.headerContainer}>
+        <Box>
+          <p className={classes.logoHeader}>Michael sulist.io </p>
+          <p className={classes.introText}>text</p>
+        </Box>
+        <PhotoSlideshow />
+      </Box>
 
-      <PhotoSlideshow />
-    </Container>
+      <Box className={classes.footer}> more to come!</Box> */}
+    </Box>
   );
 };
 
