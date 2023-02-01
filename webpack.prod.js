da3dda846f5e1f79e902b9b969e26ff33a0a0228
Michael Sulistio <michaelsulistio@gmail.com>
2023-02-01 03:22:18 -0800

    added production script

---
+++ webpack.prod.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  mode: 'production',  
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        loader: require.resolve('babel-loader'),
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: require.resolve('style-loader'),
+          },
+          {
+	    loader: require.resolve('css-loader'),
+            options: {
+              modules: true,
+              localsConvention: 'camelCase',
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      favicon: 'public/favicon.ico'
+    })
+  ],
+  resolve: {
+    plugins: [
+      PnpWebpackPlugin,
+    ],
+  },
+  resolveLoader: {
+    plugins: [
+      PnpWebpackPlugin.moduleLoader(module),
+    ],
+  },
+};
